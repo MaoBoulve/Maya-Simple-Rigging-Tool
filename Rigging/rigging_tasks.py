019f82a939e1318975e3939a5b6b9b3800eeb41a
MaoBoulve <christianmendoza2504@gmail.com>
2024-08-20 16:32:10 -0400

    Update rigging_tasks.py
    Controller on joint task complete

--- Rigging/rigging_tasks.py
+++ Rigging/rigging_tasks.py
@@ -2,13 +2,34 @@ import pymel.core as pm
 
 
 def test():
-    print(pm.ls())
+    create_control_shape_on_joint(pm.ls(sl=True)[0])
 
 
 def create_control_shape_on_joint(joint):
     print("Create ctl")
-    # todo: create controller from a selected joint - rename
-    # todo: set controller to joint transform
+
+    # parse joint name
+    joint_name = str(joint)
+
+    # get controller name from joint name, replacing _jnt with _ctl
+    controller_name = joint_name.replace('_jnt', '')
+    controller_name = controller_name + '_ctl'
+
+    # orient controller to joint translation
+    controller_center = [pm.getAttr(joint_name + '.translateX'),
+           pm.getAttr(joint_name + '.translateY'),
+           pm.getAttr(joint_name + '.translateZ')]
+    controller_normal = [0, 0, 0]
+    shape_radius = 3.0
+
+    # create nurbs circle
+    nurbs_circle = pm.circle(name=controller_name, radius=shape_radius,
+                             center=controller_center, normal=controller_normal)
+
+    # center pivot of created circle
+    pm.xform(nurbs_circle, centerPivots=True)
+
+    return nurbs_circle
 
 def create_rig_base(rig_type):
     print("Create rig base")
